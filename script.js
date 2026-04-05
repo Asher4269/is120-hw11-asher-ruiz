1c716be395f9f46ad50663aa5f5686c3489f1bab
Asher Ruiz <asherruiz42@gmail.com>
2026-04-04 21:46:26 -0600

    Last Changes + Comments

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let favorites_div = document.querySelector(".favorites-div");
 
 let memes_obj = {};
 
-let favorite_memes = JSON.parse(localStorage.getItem("favorite_memes")) || [];
+let favorite_memes = JSON.parse(localStorage.getItem("favorite_memes")) || []; // AI helped me figure out that I can just parse this at the beginning instead of in the function itself, || or helps catch errors
 
 async function get() {
   let response = await fetch("https://api.imgflip.com/get_memes");
@@ -23,9 +23,11 @@ async function get() {
 
   localStorage.setItem("memes_array", JSON.stringify(data));
 
+  // Here im getting rid of the CALL API button to not let the user call it too much. Will probably need to figure out how to make it permanently disappear based on user's local storage?
   api_caller.style.opacity = "0";
   api_caller.style.pointerEvents = "none";
 
+  // helps automatically start meme display process
   main();
 }
 
@@ -45,6 +47,7 @@ async function main() {
     };
   }
 
+  // Was not sure if I needed to do this, but this helped the page run a little smoother
   play_bracket();
   render_favorites();
 }
@@ -53,20 +56,20 @@ function play_bracket() {
   card1.innerHTML = "";
   card2.innerHTML = "";
 
-  let keys = Object.keys(memes_obj);
+  let meme_keys = Object.keys(memes_obj);
 
-  let index1 = Math.floor(Math.random() * keys.length);
-  let index2 = Math.floor(Math.random() * keys.length);
+  let index1 = Math.floor(Math.random() * meme_keys.length);
+  let index2 = Math.floor(Math.random() * meme_keys.length);
 
   while (index2 === index1) {
-    index2 = Math.floor(Math.random() * keys.length);
+    index2 = Math.floor(Math.random() * meme_keys.length);
   }
 
-  createCard(card1, memes_obj[index1]);
-  createCard(card2, memes_obj[index2]);
+  create_card(card1, memes_obj[index1]);
+  create_card(card2, memes_obj[index2]);
 }
 
-function createCard(card, meme, isFavorite = false) {
+function create_card(card, meme, isFavorite = false) {
   card.innerHTML = "";
 
   let img = document.createElement("img");
@@ -85,6 +88,7 @@ function createCard(card, meme, isFavorite = false) {
     favorite_add.textContent = "LIKE";
     favorite_add.classList.add("like");
 
+    // Could not figure out for the life of me how to do this with an event listener
     favorite_add.onclick = () => {
       add_favorite(meme);
     };
@@ -103,11 +107,12 @@ function add_favorite(meme) {
 function render_favorites() {
   favorites_div.innerHTML = "";
 
+  // Chat taught me .forEach and has made looping over an array a lot more simple
   favorite_memes.forEach((meme) => {
     let new_card = document.createElement("div");
     new_card.classList.add("card");
 
-    createCard(new_card, meme, true);
+    create_card(new_card, meme, true);
     favorites_div.appendChild(new_card);
   });
 
@@ -116,7 +121,7 @@ function render_favorites() {
 
 function clear_favorites() {
   favorite_memes = [];
-  localStorage.removeItem("favorite_memes");
+  localStorage.removeItem("favorite_memes"); // Ai taught me .removeItem, although I think we mentioned this in class too
   render_favorites();
 }
 
@@ -136,8 +141,8 @@ function check_favorites_div() {
   }
 }
 
-check_favorites_div();
 render_favorites();
+check_favorites_div();
 
 api_caller.addEventListener("click", get);
 play_button.addEventListener("click", main);
